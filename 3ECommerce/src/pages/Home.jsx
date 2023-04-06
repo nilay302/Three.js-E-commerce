bc881e1a6ee5c83c4b1ea8cc74a57c9f6377f543
nilay <nilayshirke52@gmail.com>
2023-04-06 11:14:52 +0530

    functionalities of color and logo

--- 3ECommerce/src/pages/Home.jsx
+++ 3ECommerce/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -18,7 +19,7 @@ const Home = () => {
         <motion.section className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
             <img
-              src="./three.js"
+              src="../../public/threejs.png"
               alt="logo"
               className="w-8 h-8 object-contain"
             />
@@ -30,23 +31,23 @@ const Home = () => {
                 LET'S <br className="xl:block hidden" /> DO IT.
               </h1>
             </motion.div>
-            {/* <motion.div
+            <motion.div
               {...headContentAnimation}
               className="flex flex-col gap-5"
-            > */}
-              {/* <p className="max-w-md font-normal text-gray-600 text-base">
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
                 Create your unique and exclusive shirt with our brand-new 3D
                 customization tool. <strong>Unleash your imagination</strong>{" "}
                 and define your own style.
-              </p> */}
+              </p>
 
-              {/* <CustomButton
+              <CustomButton
                 type="filled"
                 title="Customize It"
                 handleClick={() => (state.intro = false)}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-              /> */}
-            {/* </motion.div> */}
+              />
+            </motion.div>
           </motion.div>
         </motion.section>
       )}
